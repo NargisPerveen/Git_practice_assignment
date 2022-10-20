4b88427344a6171dfd3165e12d32d4ac1520699e
Nargis Perveen <nargisp360@gmail.com>
2022-10-20 12:12:35 +0530

    prime and palindrome

---
+++ plindrome.js
@@ -0,0 +1,26 @@
+//prime problem
+let num=13;
+let factor=0;
+for(let i=0;i<=num;i++){
+    if(num%i==1){
+        factor++
+    }
+}if(factor==2){
+    console.log(num,"is prime")
+}else{
+    console.log(num,"is not prime")
+}
+
+// palindrome number
+
+let num =1221;
+let rev=0;
+let rem;
+
+while(num!=0){
+rem=num%10;
+rev=rev*10+rem;
+num=Math.floor(num/10);
+}
+
+console.log(num)
